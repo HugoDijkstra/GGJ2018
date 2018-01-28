af9973028fe4a2a0af0b7920f857dc76c4c8d339
sidney dijkstra <sidney dijkstra>
2018-01-28 06:35:02 +0100

    added virus manager

---
+++ ParasiteAssissin/Assets/Scripts/Virus/AIVirus.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AIVirus : MonoBehaviour {
+    //Percent how much the entity is infected
+    private float infectPercent;
+    //The bar to show how much the entity is infected
+    public GameObject virusMeter;
+
+    // Use this for initialization
+    void Start () {
+        infectPercent = 0.0f;
+        //Adding this entity to the manager his list of infected entity's
+        InfectManager.addEntity (this);
+        infectEntity (0.15f);
+        virusMeter = transform.GetChild (0).GetChild (0).gameObject;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        
+    }
+
+    void infectEntity (float ia) {
+        //Wanneer aangeroepen, total infect count up, this infectPercentage up
+        infectPercent += ia;
+        if(virusMeter != null) {
+            //scaling the bar to show in the game how much he is infected
+            virusMeter.transform.localScale += new Vector3 (infectPercent, 0, 0);
+        }
+    }
+
+    //Getter for infectPercent
+    public float getInfectPercent () {
+        return infectPercent;
+    }
+
+    //void to set the alfa of the bar to let in show smooth when getting closer
+    public void setAlfa(float a) {
+        Color c = virusMeter.transform.parent.GetComponent<SpriteRenderer> ().color;
+        //setting the alfa of the virus meter
+        c.a = 1 - a;
+        virusMeter.transform.parent.GetComponent<SpriteRenderer>().color = c;
+    }
+}
---
+++ ParasiteAssissin/Assets/Scripts/Virus/InfectManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InfectManager : MonoBehaviour {
+    static public InfectManager instance;
+    //int die bijhoud hoeveel entity's zijn geinfect
+    private int totalInfected;
+    ArrayList entityViruslist;
+    public Parasite player;
+    //de minimale disctance waar hij de virusbar moet laten zien
+    public float minDisplayDist;
+
+    // Use this for initialization
+    void Awake () {
+        // Make a static  InfectManager, make sure there's no duplicate
+        if (instance == null) {
+            instance = this;
+        } else {
+            Destroy (this);
+            return;
+        }
+        // Inititalize int
+        totalInfected = 0;
+        // Initialize ArrayList
+        instance.entityViruslist = new ArrayList();
+    }
+
+    private void Start () {
+        // Get all the entities
+        entityViruslist = AiManager.instance._aiEntitys;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        for (int i = 0; i < entityViruslist.Count; i++) {
+            //distance between player and entity
+            float dis = Vector2.Distance (((AiEntity)entityViruslist[i]).gameObject.transform.position, player.transform.position);
+            if (dis <= minDisplayDist) {
+                //call for the function setAlfa and put the distance in it
+                ((AiEntity)entityViruslist[i]).GetComponent<AIVirus> ().setAlfa (dis / 12);
+            }
+        }
+    }
+
+    public static void addEntity (AIVirus e) {
+        // Add a entity to the arraylist
+        instance.entityViruslist.Add (e);
+    }
+
+    public static void updateInfected () {
+
+    }
+    public int getInfectedAmount () {
+        //Initialize int
+        int totalInfected = 0;
+        // Get all the entities and check if it is infected
+        foreach (AIVirus e in instance.entityViruslist) {
+            if (e.getInfectPercent () > 0) {
+                totalInfected += 1;
+            }
+        }
+        // Return the total infected amount
+        return totalInfected;
+    }
+
+}
+
